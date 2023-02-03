53c16fbbdbf387328d67c45ee9814f4e88ff134e
kotbarsik96 <kotbarsik96@gmail.com>
2023-02-03 14:29:11 +0400

    дополнение ссылок

--- js/index-resolutions.js
+++ js/index-resolutions.js
@@ -21,52 +21,58 @@ const background = document.querySelector(".background"),
 
 function adaptBackground() {
     const backgroundEndpoint = getCoords(document.querySelector("#vertical-stripe")).right;
+    const svgCoords = getCoords(document.querySelector(".wrapper__svg > svg"));
 
     background.style.width = `${backgroundEndpoint}px`;
-    backgroundLeftTopCorner.style.cssText = `
-        width: ${getCoords(document.querySelector("#top-rectangle")).right}px;
-        height: ${getCoords(document.querySelector("#top-rectangle")).top}px;
-    `;
+    // backgroundLeftTopCorner.style.cssText = `
+    //     width: ${getCoords(document.querySelector("#top-rectangle")).right}px;
+    //     height: ${getCoords(document.querySelector("#top-rectangle")).top}px;
+    // `;
 
-    const verticalContWidth = backgroundEndpoint - backgroundLeftTopCorner.clientWidth,
-        verticalContLeft = backgroundLeftTopCorner.clientWidth,
-        verticalContBottom = backgroundLeftTopCorner.clientHeight;
-    // верхние полоски
-    setVertical(
-        ".background__top-stripes",
-        "#vertical-stripe",
-        verticalContWidth,
-        verticalContLeft - 1
-    );
-    const smallestTopStripe = document.querySelector("#smallest-v-stripe");
-    if (smallestTopStripe) {
-        const w = smallestTopStripe.getBoundingClientRect().width;
-        const verticalStripeSelector = ".background__top-stripes .vertical-stripe";
-        const diff = document.querySelectorAll(verticalStripeSelector)[2].getBoundingClientRect()
-            .width - w;
-        const firstVerticalStripe = document.querySelector(verticalStripeSelector);
-        const oldShift = parseInt(firstVerticalStripe.getAttribute("x"));
+    // const verticalContWidth = backgroundEndpoint - backgroundLeftTopCorner.clientWidth,
+    //     verticalContLeft = backgroundLeftTopCorner.clientWidth,
+    // verticalContBottom = backgroundLeftTopCorner.clientHeight;
+    // // верхние полоски
+    // setVertical(
+    //     ".background__top-stripes",
+    //     "#vertical-stripe",
+    //     verticalContWidth,
+    //     verticalContLeft - 1
+    // );
+    // document.querySelector(".background__top-stripes")
+    //     .style.height = `${svgCoords.top}px`;
+    // const smallestTopStripe = document.querySelector("#smallest-v-stripe");
+    // if (smallestTopStripe) {
+    //     const w = smallestTopStripe.getBoundingClientRect().width;
+    //     const verticalStripeSelector = ".background__top-stripes .vertical-stripe";
+    //     const diff = document.querySelectorAll(verticalStripeSelector)[2].getBoundingClientRect()
+    //         .width - w;
+    //     const firstVerticalStripe = document.querySelector(verticalStripeSelector);
 
-        firstVerticalStripe.setAttribute("width", w + diff * 2);
-    }
+    //     firstVerticalStripe.setAttribute("width", w + diff * 2);
+    // }
 
-    // нижние полоски
-    setVertical(
-        ".background__bottom-stripes",
-        "#vertical-stripe-bottom",
-        verticalContWidth,
-        verticalContLeft - 1.5
-    );
+    // // нижние полоски
+    // setVertical(
+    //     ".background__bottom-stripes",
+    //     "#vertical-stripe-bottom",
+    //     verticalContWidth,
+    //     verticalContLeft - 1.5
+    // );
+    // const windowHeight = document.documentElement.clientHeight || window.innerHeight;
+    // document.querySelector(".background__bottom-stripes")
+    //     .style.height = `${windowHeight - svgCoords.bottom}px`;
 
+    // горизонтальные полоски
     const horizontalHeight = getCoords(document.querySelector("#horizontal-stripe")).bottom;
     setHorizontal(
         ".background__left-stripes",
         "#horizontal-stripe",
-        horizontalHeight - verticalContBottom,
-        verticalContBottom
+        svgCoords.bottom - svgCoords.top,
+        svgCoords.top
     );
     document.querySelector(".background__left-stripes")
-        .style.width = getCoords(document.querySelector(".wrapper__svg > svg")).left + 10;
+        .style.width = `${svgCoords.left + 10}px`;
 
 
     function setVertical(containerSelector, stripeSelector, containerWidth, leftPosition) {
@@ -97,20 +103,20 @@ function adaptBackground() {
     }
 }
 
-function setLinks(){
-    const monday = document.querySelector("#monday-link");
-    const tuesday = document.querySelector("#tuesday-link");
-    const wednesday = document.querySelector("#wednesday-link");
-    const thursday = document.querySelector("#thursday-link");
-    const friday = document.querySelector("#friday-link");
+function setLinks() {
+    const mondays = document.querySelectorAll(".monday-link"),
+        tuesdays = document.querySelectorAll(".tuesday-link"),
+        wednesdays = document.querySelectorAll(".wednesday-link"),
+        thursdays = document.querySelectorAll(".thursday-link"),
+        fridays = document.querySelectorAll(".friday-link");
 
-    monday.addEventListener("click", () => routeTo("1/"));
-    tuesday.addEventListener("click", () => routeTo("2/"));
-    wednesday.addEventListener("click", () => routeTo("3/"));
-    thursday.addEventListener("click", () => routeTo("4/"));
-    friday.addEventListener("click", () => routeTo("5/"));
+    mondays.forEach(m => m.addEventListener("click", () => routeTo("1/")));
+    tuesdays.forEach(t => t.addEventListener("click", () => routeTo("2/")));
+    wednesdays.forEach(w => w.addEventListener("click", () => routeTo("3/")));
+    thursdays.forEach(th => th.addEventListener("click", () => routeTo("4/")));
+    fridays.forEach(f => f.addEventListener("click", () => routeTo("5/")));
 
-    function routeTo(path){
+    function routeTo(path) {
         const link = document.createElement("a");
         link.style.cssText = "visibility: hidden; position: absolute;";
         link.setAttribute("href", `/vsepoint/${path}`);
